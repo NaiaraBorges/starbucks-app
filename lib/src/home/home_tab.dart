e84ff47509f08a97bab65514c301b20e64fcb4b8
Naiara Borges <naiarazk@gmail.com>
2023-12-31 19:12:18 -0300

    Home page

---
+++ lib/src/home/home_tab.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Scaffold(
+          //App bar
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            title: const SizedBox(
+              child: Center(
+                child: Image(
+                  width: 54,
+                  image: AssetImage(
+                    "assets/images/logo_appbar.png",
+                  ),
+                ),
+              ),
+            ),
+            leading: IconButton(
+                onPressed: () {
+                  //open menu
+                },
+                icon: const Icon(Icons.menu_outlined),
+                color: Colors.black),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(top: 15, right: 15),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: const Badge(
+                    label: Text(
+                      '2',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                      ),
+                    ),
+                    child: Icon(
+                      Icons.shopping_cart,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          body: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(right: 200, top: 30),
+                child: Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Welcome 👋',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              const Padding(
+                padding: EdgeInsets.only(right: 100, top: 5),
+                child: Text(
+                  'Let’s order and enjoy your order now.',
+                  style: TextStyle(fontSize: 12),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              //Campo de pesquisas
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      isDense: true,
+                      hintText: 'Pesquisar',
+                      hintStyle: TextStyle(
+                        color: Colors.grey.shade600,
+                        fontSize: 14,
+                      ),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.grey.shade600,
+                        size: 21,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(60),
+                          borderSide: const BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ))),
+                ),
+              ),
+            ],
+          )
+
+          //Categorias
+
+          //Grid
+          ),
+    );
+  }
+}
